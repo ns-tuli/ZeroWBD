b994db10a5b96b53008fb7e0d9ebebc1f9bc852f
nstuli <nusratsiddiquetuli@gmail.com>
2024-12-04 02:53:54 +0600

    header, action ,layout

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -3,9 +3,13 @@
 import { useState, useEffect } from "react"
 import { Inter } from 'next/font/google'
 import "./globals.css"
-// header
-//sidebar
+import Header from "@/components/header"
+//import Sidebar from "@/components/Sidebar"
+//import 'leaflet/dist/leaflet.css'
 import { Toaster } from 'react-hot-toast'
+import { DropdownMenu } from "@/components/ui/dropdown-menu";
+
+import { getAvailableRewards, getUserByEmail } from '@/utils/db/actions'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="min-h-screen bg-gray-50 flex flex-col">
-          //header
+        <Header onMenuClick={() => setSidebarOpen(!sidebarOpen)} totalEarnings={totalEarnings} />
           <div className="flex flex-1">
             //sidebar
             <main className="flex-1 p-4 lg:p-8 ml-0 lg:ml-64 transition-all duration-300">
--- my-app/utils/db/dbConfig.jsx
+++ my-app/utils/db/dbConfig.jsx
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
 const sql = neon(
-  process.env.DATABASE_URL
+  'postgresql://zerowbd_owner:xrPbkBjoi7D6@ep-weathered-glitter-a15z1a2w.ap-southeast-1.aws.neon.tech/zerowbd?sslmode=require'
 );
 
 export const db = drizzle(sql, { schema });
