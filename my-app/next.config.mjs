2682da6b586ba8ce954d7d4cf7bb3f3f2f9a65e6
nstuli <nusratsiddiquetuli@gmail.com>
2024-12-11 04:56:08 +0600

    collect and report

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -7,10 +7,10 @@ const nextConfig = {
       ignoreDuringBuilds: true,
     },
     env:{
-        DATABASE_URL: process.env.DATABASE_URL,
-        WEB3_AUTH_CLIENT_ID:process.env.WEB3_AUTH_CLIENT_ID,
-        GEMINI_API_KEY:process.env.GEMINI_API_KEY,
-        GOOOGLE_MAPS_API_KEY:process.env.GOOOGLE_MAPS_API_KEY
+      DATABASE_URL:'postgresql://zerowbd_owner:xrPbkBjoi7D6@ep-weathered-glitter-a15z1a2w.ap-southeast-1.aws.neon.tech/zerowbd?sslmode=require',
+      WEB3_AUTH_CLIENT_ID : 'BAwmI6f037AfJf7Vhli2fPFZkLq9ZxxlUgD1LpV7zDAODOnWjo7MrbS84OmZao2en2v36JjlrHPSuT4r3Z7lh6g',
+      GEMINI_API_KEY:'AIzaSyB2iBDJHZNytthVh4arWyLY9vhvnawAGhY',
+      GOOOGLE_MAPS_API_KEY:'AIzaSyAqAo0-uDFPLPIxwKYEEO3jdBZUVu2J9V4'
     },
   };
   
